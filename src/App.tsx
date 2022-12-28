e3075077933a79e2980090a263340035caea2587
thanglq <thangle.mi@gmail.com>
2022-12-28 14:28:24 +0700

    custom icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import jsonServerProvider from "ra-data-json-server";
 import "./App.css";
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import UsersList from "./users/users";
 import PostList from "./posts/posts";
 import PostEdit from "./posts/post-edit";
 import PostCreate from "./posts/post-create";
+
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={UsersList} recordRepresentation="name" />
+      <Resource
+        name="users"
+        list={UsersList}
+        recordRepresentation="name"
+        icon={UserIcon}
+      />
       <Resource
         name="posts"
         list={PostList}
         edit={PostEdit}
         create={PostCreate}
+        icon={PostIcon}
       />
-      <Resource name="comments" list={ListGuesser} />
     </Admin>
   );
 }
