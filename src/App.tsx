196338bc6012ba39e300bccdd2321d6d33ec74f6
thanglq <thangle.mi@gmail.com>
2022-12-28 10:18:38 +0700

    custom uers list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import jsonServerProvider from "ra-data-json-server";
 import "./App.css";
 import { Admin, Resource, ListGuesser } from "react-admin";
+import UsersList from "./users/users";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={UsersList} />
       <Resource name="posts" list={ListGuesser} />
       <Resource name="comments" list={ListGuesser} />
     </Admin>
---
+++ src/users/users.tsx
@@ -0,0 +1,29 @@
+import { Datagrid, EmailField, List, SimpleList, TextField } from "react-admin";
+import { useMediaQuery, Theme } from "@mui/material";
+
+const UsersList = () => {
+  const isSmall = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+  return (
+    <List>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.name}
+          secondaryText={(record) => record.username}
+          tertiaryText={(record) => record.email}
+        />
+      ) : (
+        <Datagrid rowClick="edit">
+          <TextField source="id" />
+          <TextField source="name" />
+          <TextField source="username" />
+          <EmailField source="email" />
+          <TextField source="address.street" />
+          <TextField source="phone" />
+          <TextField source="website" />
+          <TextField source="company.name" />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
+export default UsersList;
